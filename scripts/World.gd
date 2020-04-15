e5ebe2af288438433f8325bb29d98ffade7ff7fe
Alexis Espinoza <alexis.espinoza@ug.uchile.cl>
2020-04-15 12:55:14 -0400

    Created project node tree structure

---
+++ scripts/World.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+# Signals
+
+# Variables
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
---
+++ scripts/gui/GUI.gd
@@ -0,0 +1,9 @@
+extends Control
+
+# Signals
+
+# Variables
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
